3683a9a06e2ca41829bbeb21c43952a15248aba7
ottafregu <104422433+ottafregu@users.noreply.github.com>
2022-06-01 12:11:27 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
 function scrollToTop() {
            window.scrollTo(0, 0);
         }
+function showCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:block;";
+    }
+function hideCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:none;";
+    }
