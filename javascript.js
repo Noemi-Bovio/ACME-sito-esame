0517f8aeb6c530d9d8dafa2cdebb2aa71d7659b7
ottafregu <104422433+ottafregu@users.noreply.github.com>
2022-06-10 18:20:49 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//freccina top
 function scrollToTop() {
            window.scrollTo(0, 0);
         }
@@ -5,11 +6,49 @@ function showCookie() {
       let cookie = document.querySelector('#cookie');
       cookie.style = "display:block;";
     }
+
+//cookie
 function hideCookie() {
       let cookie = document.querySelector('#cookie');
       cookie.style = "display:none;";
     }
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+
+//feedback
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
+
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    } else if(
+        e.target.parentNode.classList.contains('rating') &&
+        e.target.previousSibling &&
+        e.target.previousElementSibling.nodeName === 'IMG'
+    ) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.innerHTML
+    }
+
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-parachute-box" style="font-size:50px"></i>
+        <strong>Risposta in volo!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>Grazie per il feedback, ci sarà utile<br>per migliorare la pagina.</p>
+    `
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
 }
